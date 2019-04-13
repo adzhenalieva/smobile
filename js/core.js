fdfea42dc35140b199c47c1c7c7edc450f627c33
Aisalkyn Djenalieva <aidzhenalieva@gmail.com>
2019-04-13 14:43:35 +0600

    code refactored

--- js/core.js
+++ js/core.js
@@ -1,10 +1,24 @@
-$(document).ready(function() {
-	$('#nav-btn').click(function(){
-		$('.main-nav ul').slideToggle(700);
-		if ($(this).hasClass('not-active')) {
-			$(this).addClass('is-active').removeClass('not-active');
-		}else{
-			$('.is-active').addClass('not-active').removeClass('is-active');
-		}
-	});
+$(document).ready(function () {
+    $('#nav-btn').click(function () {
+        $('.main-nav ul').slideToggle(700);
+        if ($(this).hasClass('not-active')) {
+            $(this).addClass('is-active').removeClass('not-active');
+        } else {
+            $('.is-active').addClass('not-active').removeClass('is-active');
+        }
+    });
+
+
+    $('#main-button').click(function () {
+        $('.quick-order').addClass('is-active').removeClass('not-active');
+        if ($(this).hasClass('not-active')) {
+            $(this).addClass('is-active').removeClass('not-active');
+        } else {
+            $('.is-active').addClass('not-active').removeClass('is-active');
+        }
+    });
 });
+
+
+
+
